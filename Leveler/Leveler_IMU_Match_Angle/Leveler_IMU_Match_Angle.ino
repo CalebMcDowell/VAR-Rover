472422609ed996b905a194bc1bae56a4abb54ebd
CalebMcDowell <cjmcdowell10@gmail.com>
2022-03-15 23:40:40 -0400

    Updated leveler with 4 actuator control
    Made moveLvl() into move4Lvl()

--- Leveler/Leveler_IMU_Match_Angle/Leveler_IMU_Match_Angle.ino
+++ Leveler/Leveler_IMU_Match_Angle/Leveler_IMU_Match_Angle.ino
@@ -15,32 +15,38 @@ Code to level an IMU on top of the tripod
     // const int enF = 1;
     const int F1 = 2;
     const int F2 = 3;
-    //Back Left
+    //Back
     // const int enBL = 10;
-    const int L1 = 4;
-    const int L2 = 5;
-    //Back Right
+    const int B1 = 4;
+    const int B2 = 5;
+    //Left
     // const int enBR = 9;
-    const int R1 = 6;
-    const int R2 = 7;
+    const int L1 = 6;
+    const int L2 = 7;
+    //Right
+    // const int enBR = 9;
+    const int R1 = 8;
+    const int R2 = 9;
     
 //////IMU Sensors//////
     Adafruit_BNO055 topImu = Adafruit_BNO055(55, 0x28);
     
 //////Function Declarations//////
-void moveLvl(char ='S', unsigned char =0, int =0);
+void move4Lvl(char ='S', unsigned char =0, int =0);
 float getIMUPitch();
 float getIMURoll();
 
 
 void setup() {
     //Linear Actuators
+    pinMode(F1, OUTPUT);
+    pinMode(F2, OUTPUT);
+    pinMode(B1, OUTPUT);
+    pinMode(B2, OUTPUT);
     pinMode(L1, OUTPUT);
     pinMode(L2, OUTPUT);
     pinMode(R1, OUTPUT);
     pinMode(R2, OUTPUT);
-    pinMode(F1, OUTPUT);
-    pinMode(F2, OUTPUT);
     
     //IMU Sensor Setup
     topImu.begin();
@@ -52,17 +58,8 @@ void setup() {
 void loop() {
     unsigned char speed = 0;
     float pitch, roll;
-  
-  //Init leveler to slightly extended position
-    moveLvl('H', speed, 1);
-    delay(500);
-    moveLvl('H', speed, -1);
-    delay(5000);
-    moveLvl('H', speed, 1);
-    delay(1000);
-    moveLvl('S');
     
-  
+    move4Lvl('S');
     while(1){
       pitch = getIMUPitch();
       roll = getIMURoll();
@@ -70,68 +67,64 @@ void loop() {
       Serial.print(", ");
       Serial.println(roll);
       if(pitch>5 || pitch<-5){
-        moveLvl('P', speed, pitch);
+        move4Lvl('P', speed, pitch);
       }
       else{
-        moveLvl('S');
+        move4Lvl('S');
       }
       if(roll>5 || roll<-5){
-        moveLvl('R', speed, roll);
+        move4Lvl('R', speed, roll);
       }
       else
-        moveLvl('S');
+        move4Lvl('S');
     }
     
 }
 
-void moveLvl(char axis='S', unsigned char pace=0, int angle=0){
+void move4Lvl(char axis='S', unsigned char pace=0, int angle=0){
     //Set direction of actuators
     bool d = 1;  //positive direction
     if(angle<0) d = 0;  //negative direction
     
     //Set axis of rotation/extend/retract/stop
-      //NOTES//
-        //Extend Left Actuator
+                //NOTES//
+          //Extend Actuator
           // digitalWrite(L1,d);
           // digitalWrite(L2,!d);
-        //Extend Right Actuator
-          // digitalWrite(R1,d);
-          // digitalWrite(R2,!d);
-        //Extend Front Actuator
-          // digitalWrite(F1,d);
-          // digitalWrite(F2,!d);
     switch((int)axis){
       case 'R': //Roll
-        digitalWrite(L1,!d);
-        digitalWrite(L2,d);
-        digitalWrite(R1,d);
-        digitalWrite(R2,!d);
-        break;
-      case 'P': //Pitch
-        digitalWrite(L1,!d);
-        digitalWrite(L2,d);
+        digitalWrite(L1,d);
+        digitalWrite(L2,!d);
         digitalWrite(R1,!d);
         digitalWrite(R2,d);
-        digitalWrite(F1,d);
-        digitalWrite(F2,!d);
+        break;
+      case 'P': //Pitch
+        digitalWrite(F1,!d);
+        digitalWrite(F2,d);
+        digitalWrite(B1,d);
+        digitalWrite(B2,!d);
         break;
       case 'H': //Height adjust
+        digitalWrite(F1,d);
+        digitalWrite(F2,!d);
+        digitalWrite(B1,d);
+        digitalWrite(B2,!d);
         digitalWrite(L1,d);
         digitalWrite(L2,!d);
         digitalWrite(R1,d);
         digitalWrite(R2,!d);
-        digitalWrite(F1,d);
-        digitalWrite(F2,!d);
         break;
       case 'S': //Stop
       default:
         pace = 0;
+        digitalWrite(F1,0);
+        digitalWrite(F2,0);
+        digitalWrite(B1,0);
+        digitalWrite(B2,0);
         digitalWrite(L1,0);
         digitalWrite(L2,0);
         digitalWrite(R1,0);
         digitalWrite(R2,0);
-        digitalWrite(F1,0);
-        digitalWrite(F2,0);
         break;
     }
     
