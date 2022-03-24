48f34ee9e10fa1134ea878653f24c80b7dd8baab
CalebMcDowell <cjmcdowell10@gmail.com>
2022-03-24 01:35:27 -0400

    Added PWM control for each actuator

--- Leveler/Leveler_IMU_Match_Angle/Leveler_IMU_Match_Angle.ino
+++ Leveler/Leveler_IMU_Match_Angle/Leveler_IMU_Match_Angle.ino
@@ -11,22 +11,23 @@ Code to level an IMU on top of the tripod
 #define BNO055_SAMPLERATE_DELAY_MS 100
 
 //////Linear Actuators//////
-    //Front
-    // const int enF = 1;
-    const int F1 = 2;
-    const int F2 = 3;
-    //Back
-    // const int enB = 10;
-    const int Back1 = 4;
-    const int Back2 = 5;
     //Left
-    // const int enL = 9;
-    const int L1 = 6;
-    const int L2 = 7;
+    const int enL = 9;
+    const int L1 = 4;
+    const int L2 = 5;
     //Right
-    // const int enR = 9;
-    const int R1 = 8;
-    const int R2 = 9;
+    const int enR = 10;
+    const int R1 = 6;
+    const int R2 = 7;
+    //Front
+    const int enF = 13;
+    const int F1 = 20;
+    const int F2 = 21;
+    //Back
+    const int enB = 11;
+    const int Back1 = 18;
+    const int Back2 = 19;
+    
     
 //////IMU Sensors//////
     Adafruit_BNO055 topImu = Adafruit_BNO055(55, 0x28);
@@ -40,50 +41,48 @@ void getIMUAngles(float &, float &);
 
 void setup() {
     //Linear Actuators
+    pinMode(enF, OUTPUT);
     pinMode(F1, OUTPUT);
     pinMode(F2, OUTPUT);
+    pinMode(enB, OUTPUT);
     pinMode(Back1, OUTPUT);
     pinMode(Back2, OUTPUT);
+    pinMode(enL, OUTPUT);
     pinMode(L1, OUTPUT);
     pinMode(L2, OUTPUT);
+    pinMode(enR, OUTPUT);
     pinMode(R1, OUTPUT);
     pinMode(R2, OUTPUT);
     
     //IMU Sensor Setup
     topImu.begin();
     topImu.setExtCrystalUse(true);
+
+    delay(1000);
     
-    Serial.begin(9600);
+//    Serial.begin(9600);
 }
 
 void loop() {
-    unsigned char speed = 1;
-    unsigned char angleRange = 2;
+    unsigned char speed = 100;
+    unsigned char angleRange = 1;
     float pitch, roll;
     
     move4Lvl('S');
     while(1){
       getIMUAngles(pitch, roll);
-      Serial.print(pitch);
-      Serial.print("\t");
-      Serial.println(roll);
+//      Serial.print(pitch);
+//      Serial.print("\t");
+//      Serial.println(roll);
 
-      if(pitch>angleRange || pitch<-angleRange){
+      if(pitch>angleRange || pitch<-angleRange)
         Pitch(speed, pitch);
-//        delay(250);
-//        Pitch(0);
-//        delay(1000);
-      }
       else
         Pitch(0);
-//      if(roll>angleRange || roll<-angleRange){
-//        Roll(speed, roll);
-//        delay(250);
-//        Roll(0);
-//        delay(1000);
-//      }
-//      else
-//        Roll(0);
+      if(roll>angleRange || roll<-angleRange)
+        Roll(speed, roll);
+      else
+        Roll(0);
     }
     
 }
@@ -135,9 +134,11 @@ void move4Lvl(char axis='S', unsigned char pace=0, int angle=0){
         break;
     }
     
-    // //Set speed of motors
-    // analogWrite(enA,pace);
-    // analogWrite(enB,pace);
+     //Set speed of motors
+     analogWrite(enL,pace);
+     analogWrite(enR,pace);
+     analogWrite(enF,pace);
+     analogWrite(enB,pace);
 }
 
 void Pitch(unsigned char pace=0, int angle=0){
@@ -153,14 +154,14 @@ void Pitch(unsigned char pace=0, int angle=0){
     //Set direction of movement
     bool d = 1;  //positive direction
     if(angle<0) d = 0;  //negative direction
-    digitalWrite(F1,!d);
-    digitalWrite(F2,d);
-    digitalWrite(Back1,!d);
-    digitalWrite(Back2,d);
+    digitalWrite(F1,d);
+    digitalWrite(F2,!d);
+    digitalWrite(Back1,d);
+    digitalWrite(Back2,!d);
     
-    // //Set speed of motors
-    // analogWrite(enA,pace);
-    // analogWrite(enB,pace);
+    //Set speed of motors
+    analogWrite(enF,pace);
+    analogWrite(enB,pace);
 }
 
 void Roll(unsigned char pace=0, int angle=0){
@@ -181,9 +182,9 @@ void Roll(unsigned char pace=0, int angle=0){
     digitalWrite(R1,d);
     digitalWrite(R2,!d);
     
-    // //Set speed of motors
-    // analogWrite(enA,pace);
-    // analogWrite(enB,pace);
+    //Set speed of motors
+    analogWrite(enL,pace);
+    analogWrite(enR,pace);
 }
 
 void getIMUAngles(float &newPitch, float &newRoll){
@@ -193,7 +194,7 @@ void getIMUAngles(float &newPitch, float &newRoll){
     newPitch = euler.y();   //Pitch
     newRoll = euler.z();    //Roll
 
-//    //Get quaternions and convert to roll and pitch
+//    //Getquaternions and convert to roll and pitch
 //    imu::Quaternion quat = topImu.getQuat();
 //    newRoll=atan2(2*(quat.w()*quat.x()+quat.y()*quat.z()),1-2*(quat.w()*quat.x()+quat.y()*quat.z()));
 //    newPitch=asin(2*(quat.w()*quat.y()-quat.z()*quat.x()));
@@ -203,7 +204,7 @@ void getIMUAngles(float &newPitch, float &newRoll){
 //    //For quaternions, reference: https://www.youtube.com/watch?v=S77r-P6YxAU&list=PLGs0VKk2DiYwEo-k0mjIkWXlkrJWAU4L9&index=21
 }
 
-//Caleb, check my work here, but I emulated the variables and mathematics from the video
+/*
 void PIDloop (){
    int tOLD;
    int tNEW;
@@ -250,5 +251,5 @@ void PIDloop (){
 
     //rollVal = rollServoVal + kp*rollERROR + kd*rolldERRORdt + ki*rollERRORarea;
     //pitchVal = pitchServoVal + kp*pitchERROR + kd*pitchdERRORdt + ki*pitchERRORarea;
-    
 }
+*/
